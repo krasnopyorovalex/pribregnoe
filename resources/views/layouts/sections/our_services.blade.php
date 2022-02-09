d76aaf41d2af95258c6e6f7c781c3b5c0310a4ec
sanek <krasnopyorovalex88@gmail.com>
2022-02-09 14:30:10 +0300

    update css

--- resources/views/layouts/sections/our_services.blade.php
+++ resources/views/layouts/sections/our_services.blade.php
@@ -25,12 +25,12 @@
                 </div>
             @endforeach
         </div>
-        <div class="row">
-            <div class="col-12">
-                <div class="btn__see-all">
-                    <a href="{{ route('page.show',['alias' => 'our-services']) }}" class="btn">Смотреть все услуги</a>
-                </div>
-            </div>
-        </div>
+{{--        <div class="row">--}}
+{{--            <div class="col-12">--}}
+{{--                <div class="btn__see-all">--}}
+{{--                    <a href="{{ route('page.show',['alias' => 'our-services']) }}" class="btn">Смотреть все услуги</a>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
     </div>
 </section>
